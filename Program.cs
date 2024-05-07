a95342291666109ee161497fc4cf001fa6bd423c
Alex Baxter <alexbaxter.work@outlook.com>
2024-05-07 09:50:51 -0600

    donezo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+while(true)
+{
+    Console.WriteLine("What is your birthday? (mm/dd/yyyy)");
+
+    var usrBirthday = Console.ReadLine();
+    var today = DateTime.Now;
+
+
+    DateTime realDay;
+    DateTime.TryParse(usrBirthday, out realDay);
+
+    var dayAge = today - realDay;
+
+    Console.WriteLine($" THE LORD HAS GIVEN YOU {dayAge.Days} DAYS!");
+
+    if (dayAge.Days < 26809)
+    {
+        Console.WriteLine($"you may only have {26809-dayAge.Days} left. use them well"); //number of days based on avg life expectancy of 73.4 years
+    }
+    else
+    {
+        Console.WriteLine("you beat the odds.");
+    }
+
+}
--- obj/Debug/net8.0/Final.AssemblyInfo.cs
+++ obj/Debug/net8.0/Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe59bb944229303396d9edbf2f27a7076f5c56b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d76c3f07cea23723243164a63d75106ef19b3ae2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
